568092e3ad973ad3ca353a4981ef676b95fa0114
ketanrd26 <ketandudka566123@gmail.com>
2024-09-01 12:04:36 +0530

    chnages

---
+++ src/pages/before_login/Before_login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./before.scss"
+const Before_login = () => {
+  return (
+    <>
+      <div className="home-page-parent parent">
+        <div className="home-page cont cont">
+            
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Before_login

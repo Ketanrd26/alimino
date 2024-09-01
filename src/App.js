8dd7faae959bb0f53a6cc1124c0a46235f18b1b2
ketanrd26 <ketandudka566123@gmail.com>
2024-09-01 12:36:46 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Before_login from './pages/before_login/Before_login';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+      <Routes>
+        <Route  path='/' element={<Before_login/>}  />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/before_login/Before_login.js
+++ src/pages/before_login/Before_login.js
@@ -1,15 +1,54 @@
-import React from 'react'
-import "./before.scss"
+import React from "react";
+import "../before_login/before.scss";
+
+// icons
+
+import { FaLinkedinIn } from "react-icons/fa";
 const Before_login = () => {
   return (
     <>
       <div className="home-page-parent parent">
-        <div className="home-page cont cont">
-            
+        <div className="home-page-cont cont">
+          <div className="box">
+            <div className="social-icons">
+              <div className="logo"></div>
+              <div className="icons">
+                <span>
+                  <FaLinkedinIn />
+                </span>
+                <span>
+                  <FaLinkedinIn />
+                </span>
+                <span>
+                  <FaLinkedinIn />
+                </span>
+                <span>
+                  <FaLinkedinIn />
+                </span>
+                <span>
+                  <FaLinkedinIn />
+                </span>
+              </div>
+            </div>
+            <div className="tag">
+              <h4>
+                Welcome to BVCOEW'S
+                <br />
+                Student alumini <br />
+                association
+              </h4>
+            </div>
+            <div className="login">
+              Login Your Account
+            </div>
+            <div className="register">
+              Register Your Page
+            </div>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Before_login
+export default Before_login;
